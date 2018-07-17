adcb0be37b0aed26516e243d429e2cd8eec1f1ef
Alejandro Bueno <alejandro.bueno@neueda.com>
2018-07-17 14:32:12 +0200

    Missing files.

--- src/main/java/org/blocknroll/blockchain/workshop/NodeMain.java
+++ src/main/java/org/blocknroll/blockchain/workshop/NodeMain.java
@@ -1,90 +1,262 @@
 package org.blocknroll.blockchain.workshop;
 
-import java.util.Scanner;
+import static org.blocknroll.blockchain.workshop.NodeActor.parseAddress;
 
-public class Main {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
+import akka.actor.ActorSystem;
+import akka.actor.Address;
+import akka.actor.PoisonPill;
+import akka.actor.Props;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent;
+import akka.cluster.ClusterEvent.MemberEvent;
+import akka.cluster.ClusterEvent.UnreachableMember;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import java.io.Serializable;
+import java.net.InetSocketAddress;
 
-  private final static int DEFAULT_PORT = 2551;
+public class NodeMain extends AbstractActor {
 
-  private final static int CMD_SUCCESS = 0;
-  private final static int CMD_ERROR = -1;
-  private final static int CMD_ABORT = -2;
+  // some constants
+  private final static String HELP = "\n".join(
+      "Usage:",
+      "   COMMAND [OPTIONS] [ARGUMENTS]",
+      "",
+      "Commands:",
+      "   start [PORT [PEER_NODE]]",
+      "      Starts a local node listening to PORT for commands. Optionally it joins to PEER_NODE.",
+      "   stop [NODE]",
+      "      Stops a local or remote NODE (use with care!).",
+      "   join [NODE [PEER_NODE]]",
+      "      Makes NODE to leave current cluster and to join to PEER_NODE.",
+      "   leave [NODE]",
+      "      Makes NODE to leave its current cluster but keeps it running.",
+      "   mine-facts [NODE [FACTS...]]",
+      "      Mines a new block in NODE from given facts.",
+      "   get-chain [NODE]",
+      "      Retrieves the chain from NODE.",
+      "   update-chain [NODE]",
+      "      Forces NODE to update its chain.",
+      "   status [NODE]",
+      "      Displays status information for NODE, including its current block.",
+      "",
+      "Options:",
+      "   -h\tPrints this help and exists.",
+      "",
+      "Notes:",
+      "   All NODES are in the form IP:PORT, assuming localhost when IP is not given.");
+  private final static String DEFAULT_HOST = "127.0.0.1";
+  private final static int DEFAULT_PORT = 2550;
 
-  private static int port;
-
-  private static void checkArgs(String[] args) {
-    switch (args.length) {
-      case 0:
-        port = DEFAULT_PORT;
-        System.out.println("Using default port " + port + ".");
-        break;
-      case 1:
-        port = Integer.parseInt(args[1]);
-        System.out.println("Using port " + port + ".");
-        break;
-      default:
-        System.out.println("Error: too many arguments.");
-        System.out.println("Expected port number.");
-        System.exit(-1);
-    }
+  // node comands
+  enum Command {
+    START, STOP, JOIN, LEAVE, MINE_FACTS, GET_CHAIN, UPDATE_CHAIN, STATUS
   }
 
-  private static int joinCmd(Scanner args) {
-    System.out.println("To be implemented...");
-    return CMD_ERROR;
+  // cluster objects
+  private LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+  private Cluster cluster = Cluster.get(getContext().system());
+
+  // input arguments
+  private static class Arguments implements Serializable {
+
+    public Command command = null;
+    public InetSocketAddress nodeAddress = null;
+    public InetSocketAddress peerAddress = null;
+    public String facts = null;
   }
 
-  private static int helpCmd(Scanner args) {
-    if (args.hasNext()) {
-      System.out.println("Error: too many arguments.");
-      return CMD_ERROR;
-    }
-    System.out.println("join IP PORT\tJoins to a cluster by contacting to IP:PORT.");
-    System.out.println("leave\tLeaves cluster.");
-    System.out.println("add FACTS...\tCreates a new block with input facts and starts mining.");
-    System.out.println("chain\tDisplays current chain.");
-    System.out.println("chain\tDisplays current chain.");
-    System.out.println("help\tPrints this help.");
-    System.out.println("exit\tExists the command loop and finishes the program.");
-    return CMD_SUCCESS;
+  private static String nodeName(String prefix, InetSocketAddress address) {
+    return prefix + "@" + address.getHostName() + ":" + address.getPort();
   }
 
-  private static int exitCmd(Scanner args) {
-    if (args.hasNext()) {
-      System.out.println("Error: too many arguments.");
-      return CMD_ERROR;
-    }
-    System.out.println("Bye!");
-    return CMD_ABORT;
+  public NodeMain(Arguments args)
+  {
+    System.out.println("Aquí estamos");
+    self().tell(args, self());
   }
 
-  private static int repl() {
-    int result = CMD_SUCCESS;
-    while (result != CMD_ABORT) {
-      Scanner command = new Scanner(System.in);
-      System.out.println(">>");
-      switch (command.next().toUpperCase()) {
-        case "join":
-          result = joinCmd(command);
-          break;
-        case "help":
-          result = helpCmd(command);
-          break;
-        case "exit":
-          result = exitCmd(command);
-          break;
-        default:
-          System.out.println("Error: command not found.");
-          result = CMD_ERROR;
-          break;
+  @Override
+  public void preStart() {
+    cluster.subscribe(self(), ClusterEvent.initialStateAsEvents(), MemberEvent.class,
+        UnreachableMember.class);
+  }
+
+  @Override
+  public void postStop() {
+    cluster.unsubscribe(self());
+  }
+
+  @Override
+  public Receive createReceive() {
+    return receiveBuilder()
+        .match(String.class, msg -> {
+              System.out.println("-------------------------------------------------");
+              System.out.println(msg);
+              System.out.println("-------------------------------------------------");
+            }
+        )
+        .match(Arguments.class, msg -> {
+          // cluster.join(msg.nodeAddress);
+          String nodeName = nodeName("node", msg.nodeAddress);
+          switch (msg.command) {
+            case START:
+              ActorRef node = getContext().actorOf(Props.create(NodeActor.class));
+              //ActorRef node = NodeActor.startup(msg.nodeAddress);
+              if (msg.peerAddress != null) {
+                node.tell(
+                    new NodeMessages.Join(msg.peerAddress.getHostName(), msg.peerAddress.getPort()),
+                    getSelf());
+              }
+              break;
+
+            case STOP: {
+              ActorSelection selection = getContext().actorSelection(nodeName);
+              selection.tell(PoisonPill.getInstance(), getSelf());
+              break;
+            }
+          }
+        })
+        .build();
+  }
+
+  private static Arguments checkArgs(String[] args) {
+    Arguments result = new Arguments();
+
+    try {
+      for (int i = 0; i < args.length; i++) {
+        if (args[i].equalsIgnoreCase("-h")) {
+          System.out.println(HELP);
+          System.exit(0);
+        } else if (result.command == null) {
+          if (args[i].equalsIgnoreCase("start")) {
+            result.command = Command.START;
+          } else if (args[i].equalsIgnoreCase("stop")) {
+            result.command = Command.STOP;
+          } else if (args[i].equalsIgnoreCase("join")) {
+            result.command = Command.JOIN;
+          } else if (args[i].equalsIgnoreCase("leave")) {
+            result.command = Command.LEAVE;
+          } else if (args[i].equalsIgnoreCase("mine-facts")) {
+            result.command = Command.MINE_FACTS;
+          } else if (args[i].equalsIgnoreCase("get-chain")) {
+            result.command = Command.GET_CHAIN;
+          } else if (args[i].equalsIgnoreCase("update-chain")) {
+            result.command = Command.UPDATE_CHAIN;
+          } else if (args[i].equalsIgnoreCase("status")) {
+            result.command = Command.STATUS;
+          } else {
+            new RuntimeException("unknown command " + args[i]);
+          }
+        } else if (result.command == Command.START || result.command == Command.JOIN) {
+          if (result.nodeAddress == null) {
+            result.nodeAddress = parseAddress(args[i]);
+          } else if (result.peerAddress == null) {
+            result.peerAddress = parseAddress(args[i]);
+          } else {
+            throw new RuntimeException("too many arguments");
+          }
+        } else if (result.command == Command.STOP || result.command == Command.LEAVE
+            || result.command == Command.GET_CHAIN || result.command == Command.UPDATE_CHAIN
+            || result.command == Command.STATUS) {
+          if (result.nodeAddress == null) {
+            result.nodeAddress = parseAddress(args[i]);
+          } else {
+            throw new RuntimeException("too many arguments");
+          }
+        } else if (result.command == Command.LEAVE) {
+          if (result.nodeAddress == null) {
+            result.nodeAddress = parseAddress(args[i]);
+          } else {
+            throw new RuntimeException("too many arguments");
+          }
+        } else if (result.command == Command.MINE_FACTS) {
+          if (result.nodeAddress == null) {
+            result.nodeAddress = parseAddress(args[i]);
+          } else if (result.facts == null) {
+            result.facts = args[i];
+          } else {
+            result.facts += " " + args[i];
+          }
+        } else {
+          throw new RuntimeException("too many arguments");
+        }
       }
-      command.close();
+      if (result.command == null) {
+        throw new RuntimeException("expected command");
+      }
+
+      // defaults
+      result.nodeAddress = (result.nodeAddress == null ? parseAddress(null) : result.nodeAddress);
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage() + ".");
+      System.out.println("Try -h for help.");
+      System.exit(-1);
     }
     return result;
   }
 
   public static void main(String[] args) {
-    checkArgs(args);
-    System.exit(repl());
+    Arguments arguments = checkArgs(args);
+    //    System.out.println("COMMAND: " + command);
+    //    System.out.println("NODE: " + nodeAddress.toString());
+    //    System.out.println("PEER: " + (peerAddress != null ? peerAddress.toString() : "<unknown>"));
+    //    //System.exit(-1);
+
+    // create this actor and join to our node
+    //InetSocketAddress mainAddress = new InetSocketAddress("127.0.0.1", 12345);
+    InetSocketAddress mainAddress = parseAddress(null);
+    /*
+    Config config = ConfigFactory
+        .parseString("akka.remote.netty.tcp.port=" + mainAddress.getPort() + "\n")
+        //+            "akka.remote.artery.canonical.port=" + mainAddres.getPort())
+        .withFallback(ConfigFactory.load());*/
+    ActorSystem system = ActorSystem.create("ClusterSystem");
+    String mainName = nodeName("main", mainAddress);
+    ActorRef mainNode = system.actorOf(Props.create(NodeMain.class, arguments), mainName);
+    //mainNode.tell(arguments, mainNode);
+
+    /*
+    switch (arguments.command) {
+
+      case START:
+        break;
+      }
+
+      case STOP: {
+        String nodeName = "node@" + nodeAddress.toString();
+        ActorSelection selection = context.actorSelection(nodeName);
+        selection.tell(new PoisonPill(), );
+        break;
+      }
+
+      case JOIN:
+        break;
+      case LEAVE:
+        break;
+      case MINE_FACTS:
+        break;
+      case GET_CHAIN:
+        break;
+      case UPDATE_CHAIN:
+        break;
+      case STATUS:
+        break;
+    }
+
+    / *
+    boolean connectToSeed = true;
+    System.out.println("Listening on port " + port + ".");
+    ActorRef node = startup();
+    if (connectToSeed) {
+      System.out.println("Connecting to cluster at " + peerHost + ":" + peerPort + "...");
+      node.tell(new NodeMessages.Join(peerHost, peerPort), node);
+    }
+    */
   }
 }
--- src/main/java/org/blocknroll/blockchain/workshop/NodeMessages.java
+++ src/main/java/org/blocknroll/blockchain/workshop/NodeMessages.java
@@ -1,5 +1,29 @@
 package org.blocknroll.blockchain.workshop;
 
+import java.io.Serializable;
+
 public interface NodeMessages {
 
+  public static class Join implements Serializable {
+
+    public String host;
+    public int port;
+
+    public Join(String host, int port) {
+      System.out.println("[" + host + ":" + port + "]");
+      this.host = host;
+      this.port = port;
+    }
+  }
+
+  public static class Leave implements Serializable {
+
+    public Leave() {
+    }
+  }
+
+  public static class AddFacts implements Serializable{
+    public AddFacts (String filename)
+    {}
+  }
 }
