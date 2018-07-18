7c426bb2ad7c041cb1e5750bee3ea781f1bd53b2
David Torelli <david.torelli@neueda.com>
2018-07-18 23:09:01 +0200

    Hot fix add test for Chain

---
+++ src/test/java/org/blocknroll/blockchain/workshop/ChainTest.java
@@ -0,0 +1,46 @@
+package org.blocknroll.blockchain.workshop;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import org.junit.Test;
+
+public class ChainTest {
+
+  @Test
+  public void addBlock() throws Exception {
+    Chain chain = new Chain("myChain");
+    assertEquals(0, chain.getSize());
+    chain.addBlock(new Block());
+    assertEquals(1, chain.getSize());
+  }
+
+  @Test
+  public void getLastBlock() throws Exception {
+    Chain chain = new Chain("myChain");
+    Block gen = new Block();
+    chain.addBlock(gen);
+    assertEquals(gen, chain.getLastBlock());
+  }
+
+  @Test
+  public void getBlocks() throws Exception {
+    Chain chain = new Chain("myChain");
+    Block gen = new Block();
+    chain.addBlock(gen);
+    chain.addBlock(new Block(new ArrayList<>(), gen));
+    assertEquals(2, chain.getBlocks().size());
+  }
+
+  @Test
+  public void getSize() throws Exception {
+    Chain chain = new Chain("myChain");
+    Block gen = new Block();
+    chain.addBlock(gen);
+    chain.addBlock(new Block(new ArrayList<>(), gen));
+    assertEquals(2, chain.getSize());
+  }
+
+}
